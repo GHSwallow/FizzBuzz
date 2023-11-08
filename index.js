0674718396c4ac111207eecd53702802fc29706e
GHSwallow <Gabriel.Swallow@softwire.com>
2023-11-08 11:50:49 +0000

    Added user prompt for specifying max number

--- index.js
+++ index.js
@@ -1,41 +1,59 @@
-for (let i = 1; i <= 256; i++) {
-    let startOfTextArray = [];
-    let endOfTextArray = [];
+const prompt = require("prompt-sync")();
+maxNumberToPrintTo = selectNumber()
+fizzBuzz(maxNumberToPrintTo)
 
-    if (i % 3 === 0) {
-        startOfTextArray.push('Fizz')
+function selectNumber(){
+    let input = Number(prompt("Select number to print up to (must be int)"))
+    if (isNaN(input)) {
+        console.log("input not able to convert to number, try again")
+        return selectNumber()
+    } else if (!Number.isInteger(input)) {
+        console.log("input is not an integer, try again")
+        return selectNumber()
     }
+    return input
+}
 
-    if (i % 5 === 0) {
-        endOfTextArray.push('Buzz')
-    }
+function fizzBuzz(maxNumber){
+    for (let i = 1; i <= maxNumber; i++) {
+        let startOfTextArray = [];
+        let endOfTextArray = [];
 
-    if (i % 7 === 0) {
-        endOfTextArray.push('Bang')
-    }
+        if (i % 3 === 0) {
+            startOfTextArray.push('Fizz')
+        }
 
-    if (i % 11 === 0) {
-        startOfTextArray = []
-        endOfTextArray.push('Bong')
-    }
+        if (i % 5 === 0) {
+            endOfTextArray.push('Buzz')
+        }
 
-    if (i % 13 === 0) {
-        startOfTextArray.push('Fezz')
-    }
+        if (i % 7 === 0) {
+            endOfTextArray.push('Bang')
+        }
 
-    if (i % 17 === 0) {
-        startOfTextArray.reverse()
-        endOfTextArray.reverse()
-        let temp = startOfTextArray
-        startOfTextArray = endOfTextArray
-        endOfTextArray = temp
-    }
+        if (i % 11 === 0) {
+            startOfTextArray = []
+            endOfTextArray.push('Bong')
+        }
 
-    if (startOfTextArray.length===0 && endOfTextArray.length===0){
-        console.log(i.toString())
-    } else {
-        let startText = String(startOfTextArray).replaceAll(",", "")
-        let endText = String(endOfTextArray).replaceAll(",", "")
-        console.log(startText + endText)
+        if (i % 13 === 0) {
+            startOfTextArray.push('Fezz')
+        }
+
+        if (i % 17 === 0) {
+            startOfTextArray.reverse()
+            endOfTextArray.reverse()
+            let temp = startOfTextArray
+            startOfTextArray = endOfTextArray
+            endOfTextArray = temp
+        }
+
+        if (startOfTextArray.length===0 && endOfTextArray.length===0){
+            console.log(i.toString())
+        } else {
+            let startText = String(startOfTextArray).replaceAll(",", "")
+            let endText = String(endOfTextArray).replaceAll(",", "")
+            console.log(startText + endText)
+        }
     }
 }
